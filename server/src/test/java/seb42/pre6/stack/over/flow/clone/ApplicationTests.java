5eec637731a32948496ac08740536743a634781d
[minso kim] <[bella.minsokim@gmail.com]>
2023-02-15 17:35:45 +0900

    feat:env 개발 환경 세팅하기

---
+++ server/src/test/java/seb42/pre6/stack/over/flow/clone/ApplicationTests.java
@@ -0,0 +1,13 @@
+package seb42.pre6.stack.over.flow.clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
