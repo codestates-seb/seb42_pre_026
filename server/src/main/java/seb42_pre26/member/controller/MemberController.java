00f8c697419a94ff614438ecdf223595e39237b2
vxy2715 <vxy2715@gmail.com>
2023-02-20 10:48:50 +0900

    feat : member 회원 가입 기능 구현

---
+++ server/src/main/java/seb42_pre26/member/controller/MemberController.java
@@ -0,0 +1,29 @@
+package seb42_pre26.member.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import seb42_pre26.member.dto.MemberSignUpDto;
+import seb42_pre26.member.service.MemberService;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/sign-up")
+    public String signUp(@RequestBody MemberSignUpDto memberSignUpDto) throws Exception {
+        memberService.signUp(memberSignUpDto);
+        return "회원가입 성공";
+    }
+
+    @GetMapping("/jwt-test")
+    public String jwtTest() {
+        return "jwtTest 요청 성공";
+    }
+
+}
