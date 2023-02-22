f575c334cec3839ec74d108607f2e67f74134f76
soohyuneee <tlstngus95@naver.com>
2023-02-22 17:28:53 +0900

    feat : 컴펌 추가

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Question from './pages/Question';
 import { createGlobalStyle } from 'styled-components';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import { ConfirmProvider } from 'material-ui-confirm';
 import 'react-toastify/dist/ReactToastify.min.css';
 
 const GlobalStyle = createGlobalStyle`
@@ -21,27 +22,29 @@ button {
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <GlobalStyle />
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/new" element={<New />} />
-          <Route path="/question/:id" element={<Question />} />
-        </Routes>
-        <ToastContainer
-          style={{ zIndex: 20 }}
-          position="top-right"
-          hideProgressBar={false}
-          autoClose={3000}
-          closeOnClick
-          pauseOnFocusLoss
-          theme="light"
-        />
-      </div>
-    </BrowserRouter>
+    <ConfirmProvider>
+      <BrowserRouter>
+        <div className="App">
+          <GlobalStyle />
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/new" element={<New />} />
+            <Route path="/question/:id" element={<Question />} />
+          </Routes>
+          <ToastContainer
+            style={{ zIndex: 20 }}
+            position="top-right"
+            hideProgressBar={false}
+            autoClose={3000}
+            closeOnClick
+            pauseOnFocusLoss
+            theme="light"
+          />
+        </div>
+      </BrowserRouter>
+    </ConfirmProvider>
   );
 }
 
