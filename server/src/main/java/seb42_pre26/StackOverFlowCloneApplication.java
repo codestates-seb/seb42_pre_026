9723e18c4c21f8bb8ee0965b69cadee2db153534
[minso kim] <[bella.minsokim@gmail.com]>
2023-03-01 22:47:23 +0900

    chore : 설정

--- server/src/main/java/seb42_pre26/StackOverFlowCloneApplication.java
+++ server/src/main/java/seb42_pre26/StackOverFlowCloneApplication.java
@@ -2,14 +2,21 @@ package seb42_pre26;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class StackOverFlowCloneApplication {
+public class StackOverFlowCloneApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		SpringApplication.run(StackOverFlowCloneApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(StackOverFlowCloneApplication.class);
 	}
 
 }
--- server/src/main/java/seb42_pre26/global/config/WebConfig.java
+++ server/src/main/java/seb42_pre26/global/config/WebConfig.java
@@ -15,11 +15,11 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("*")
+                         .allowedOriginPatterns("*")
                         .allowedHeaders("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS" , "PATCH")
-                        .exposedHeaders("Authorization", "RefreshToken");
-                //.allowCredentials(true); // .allowedOriginPatterns("*") 이렇게 와일드 카드로 설정하면 이거 쓰면 에러남 ( 실행 조차  X )
+                        .exposedHeaders("Authorization", "RefreshToken")
+                .allowCredentials(true); // .allowedOriginPatterns("*") 이렇게 와일드 카드로 설정하면 이거 쓰면 에러남 ( 실행 조차  X )
             }
         };
     }
