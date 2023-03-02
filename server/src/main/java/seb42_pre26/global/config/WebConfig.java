c6346460d7bf898ffe14e8dd7e75f8715b083b0f
zkaryaJo <gudwls1029@naver.com>
2023-03-02 16:36:10 +0900

    fix: webconfig 수정git add  server/src/main/java/seb42_pre26/global/config/WebConfig.javagit add  server/src/main/java/seb42_pre26/global/config/WebConfig.javagit add  server/src/main/java/seb42_pre26/global/config/WebConfig.javagit add  server/src/main/java/seb42_pre26/global/config/WebConfig.java

--- server/src/main/java/seb42_pre26/global/config/WebConfig.java
+++ server/src/main/java/seb42_pre26/global/config/WebConfig.java
@@ -9,19 +9,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                         .allowedOriginPatterns("*")
-                        .allowedHeaders("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS" , "PATCH")
-                        .exposedHeaders("Authorization", "RefreshToken")
-                .allowCredentials(true); // .allowedOriginPatterns("*") 이렇게 와일드 카드로 설정하면 이거 쓰면 에러남 ( 실행 조차  X )
-            }
-        };
+ @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+        	.allowedOrigins("http://localhost:3000")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS" , "PATCH")
+            .allowCredentials(true);
     }
 
 
